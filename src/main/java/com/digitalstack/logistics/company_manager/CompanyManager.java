2cd99a61d51805de170f75c03f5670dd81dcebfa
stefanviorel.popescu@gmail.com <stefanviorel.popescu@gmail.com>
2023-05-04 11:28:54 +0300

    [LS-04] - Add support for current date and company profit.

---
+++ src/main/java/com/digitalstack/logistics/company_manager/CompanyManager.java
@@ -0,0 +1,32 @@
+package com.digitalstack.logistics.company_manager;
+
+import lombok.Getter;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+
+@Component
+@Getter
+public class CompanyManager
+{
+    private LocalDate currentDate = LocalDate.of(2021, 12, 14);
+
+    private Long companyProfit;
+
+    /**
+     * Advances the currentDate by one day
+     */
+    public void advanceDate() {
+        currentDate = currentDate.plusDays(1);
+    }
+
+    /**
+     * Update profit with the given amount
+     * @param amount amount to add
+     */
+    public synchronized void updateProfit(long amount) {
+        companyProfit += amount;
+    }
+
+}
+
