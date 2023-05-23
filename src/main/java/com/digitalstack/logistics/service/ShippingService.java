d8fb88035334c1e830100543cd17a14ac517becd
stefanviorel.popescu@gmail.com <stefanviorel.popescu@gmail.com>
2023-05-23 11:31:45 +0300

    [LS-10] - Add “shippingExecutor” to the application. We should have 4 active threads and 100 queue size. Process orders and update company profit.
    Added SL4J logging.

--- src/main/java/com/digitalstack/logistics/service/ShippingService.java
+++ src/main/java/com/digitalstack/logistics/service/ShippingService.java
@@ -1,9 +1,12 @@
 package com.digitalstack.logistics.service;
 
 import com.digitalstack.logistics.company_manager.CompanyManager;
+import com.digitalstack.logistics.helpers.OrderStatus;
+import com.digitalstack.logistics.model.converter.OrderConverter;
 import com.digitalstack.logistics.model.entity.Destination;
 import com.digitalstack.logistics.model.entity.Order;
 import com.digitalstack.logistics.repository.OrdersRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
@@ -22,6 +25,7 @@ import java.util.stream.Collectors;
  * 3.  Async process the orders for each destination
  */
 @Service
+@Slf4j
 public class ShippingService
 {
     private final CompanyManager companyManager;
@@ -44,10 +48,16 @@ public class ShippingService
     public String newDay()
     {
         companyManager.advanceDate();
+        log.info("New Day Starting: {}", companyManager.getCurrentDate().format(OrderConverter.formatter));
 
         Map<Destination, List<Order>> ordersByDestination = getOrdersByDestination();
+        String destinationsString = ordersByDestination.keySet().stream()
+                .map(Destination::getName)
+                .collect(Collectors.joining(","));
+        log.info("Today we will be delivering to {}", destinationsString);
 
-        ordersByDestination.entrySet().forEach(destinationListEntry -> executor.execute(new ShippingRunnable(destinationListEntry)));
+        ordersByDestination.entrySet().forEach(destinationListEntry ->
+                executor.execute(new ShippingRunnable(ordersRepository, companyManager, destinationListEntry)));
 
         int numberOfOrders = ordersByDestination.values().stream()
                 .map(List::size)
@@ -62,6 +72,7 @@ public class ShippingService
         long currentDateAsLong = currentDate.atStartOfDay().toInstant(ZoneOffset.UTC).toEpochMilli();
         List<Order> orders = ordersRepository.findAllByDeliveryDate(currentDateAsLong);
         return orders.stream()
+                .filter(order -> order.getStatus() == OrderStatus.NEW)
                 .collect(Collectors.groupingBy(Order::getDestination, Collectors.toList()));
     }
 }
